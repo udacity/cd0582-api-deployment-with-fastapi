c4d5b7e6f48442a0bf3800b9afa327c1b3bad805
ronny-udacity <ronny@udacity.com>
2023-01-23 14:31:30 -0800

    POST items

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-# 3 - Intro to FastAPI
+# Import Union since our Item object will have tags that can be strings or a list.
+from typing import Union
+
 from fastapi import FastAPI
+# BaseModel from Pydantic is used to define data objects.
+from pydantic import BaseModel
+
+# Declare the data object with its components and their type.
+class TaggedItem(BaseModel):
+    name: str
+    tags: Union[str, list]
+    item_id: int
 
 app = FastAPI()
 
-@app.get("/")
-async def say_hello():
-  return {"greeting": "Hello World!"}
+# This allows sending of data (our TaggedItem) via POST to the API.
+@app.post("/items/")
+async def create_item(item: TaggedItem):
+    return item
